b19a71255058bca5f832fce0dc26cf5206af87ad
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-26 21:13:04 -0300

    feat(Page): implement base login form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,69 @@
-import Image from "next/image";
+"use client";
+
+import { Form } from "@/components/Form";
+import { FormProvider, useForm } from "react-hook-form";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  /* 
+  inicializo o gerenciamento do formulário 
+  useForm para usar os métodos e tipagem (zod e @hookform/resolvers/zod)
+  */
+  const loginForm = useForm({
+    // TODO adicionar resolver com e tipagem com zod
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  // função que vai ser chamada quando o formulário for enviado
+  const handleSubmitLogin = (data: unknown) => {
+    // TODO adicionar função
+    console.log(data);
+  };
+
+  return (
+    <>
+      {/* 
+      FormProvider para usar o contexto do react hook form e conectar aos componentes filhos com o useForm via spread (...)
+      */}
+      <FormProvider {...loginForm}>
+        <div className="flex  flex-col items-center mt-24">
+          {/* 
+          Valida os dados  e chama a função de submit 
+          */}
+          <Form.Root onSubmit={loginForm.handleSubmit(handleSubmitLogin)}>
+            <Form.Field>
+              <Form.Label htmlFor="nome">Nome</Form.Label>
+              <Form.Input
+                name="nome"
+                type="text"
+                placeholder="Informe seu nome..."
+              />
+            </Form.Field>
+          </Form.Root>
+
+          <Form.Root>
+            <Form.Field className="mt-4">
+              <Form.Label htmlFor="email">Email</Form.Label>
+              <Form.Input
+                name="email"
+                type="email"
+                placeholder="Informe seu email..."
+              />
+            </Form.Field>
+          </Form.Root>
+
+          <Form.Root>
+            <Form.Field className="mt-4">
+              <Form.Label htmlFor="senha">Senha</Form.Label>
+              <Form.Input
+                name="senha"
+                type="password"
+                placeholder="Informe sua senha...."
+              />
+            </Form.Field>
+          </Form.Root>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </FormProvider>
+
+      {/* TODO adicionar botões */}
+    </>
   );
 }
