04eaa1f479fad49db7bbb2d6784102281809e040
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-29 18:43:11 -0300

    feat: implement cadastro form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
 "use client";
 
+import { Button } from "@/components/Button";
 import { Form } from "@/components/Form";
+import {
+  cadastroSchema,
+  type cadastroSchemaProps,
+} from "@/schemas/cadastroSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useTransition } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 
 export default function Home() {
-  /* 
-  inicializo o gerenciamento do formulário 
-  useForm para usar os métodos e tipagem (zod e @hookform/resolvers/zod)
-  */
-  const loginForm = useForm({
-    // TODO adicionar resolver com e tipagem com zod
+  const [isSaving, startIsSaving] = useTransition(); // useTransition usado para exibir loading enquanto o cadastro está sendo processado, sem travar a interface.
+
+  // inicializo o gerenciamento do formulário com useForm para usar os métodos e tipagem (cadastroSchemaProps) e @hookform/resolvers/zod
+  const cadastroForm = useForm<cadastroSchemaProps>({
+    resolver: zodResolver(cadastroSchema),
   });
 
   // função que vai ser chamada quando o formulário for enviado
-  const handleSubmitLogin = (data: unknown) => {
-    // TODO adicionar função
-    console.log(data);
+  const handleSubmitLogin = (data: cadastroSchemaProps) => {
+    startIsSaving(async () => {
+      await new Promise((resolver) => setTimeout(resolver, 2000)); // simula delay
+      console.log(data); // aqui teria um await e chamaria uma função real de cadastro
+      cadastroForm.reset(); // limpa o formulário após cadastrar
+    });
   };
 
   return (
@@ -23,23 +32,19 @@ export default function Home() {
       {/* 
       FormProvider para usar o contexto do react hook form e conectar aos componentes filhos com o useForm via spread (...)
       */}
-      <FormProvider {...loginForm}>
+      <FormProvider {...cadastroForm}>
         <div className="flex  flex-col items-center mt-24">
-          {/* 
-          Valida os dados  e chama a função de submit 
-          */}
-          <Form.Root onSubmit={loginForm.handleSubmit(handleSubmitLogin)}>
+          <Form.Root onSubmit={cadastroForm.handleSubmit(handleSubmitLogin)}>
             <Form.Field>
-              <Form.Label htmlFor="nome">Nome</Form.Label>
+              <Form.Label htmlFor="name">Nome</Form.Label>
               <Form.Input
-                name="nome"
+                name="name"
                 type="text"
                 placeholder="Informe seu nome..."
               />
+              <Form.ErrorMessage field="name" />
             </Form.Field>
-          </Form.Root>
 
-          <Form.Root>
             <Form.Field className="mt-4">
               <Form.Label htmlFor="email">Email</Form.Label>
               <Form.Input
@@ -47,23 +52,26 @@ export default function Home() {
                 type="email"
                 placeholder="Informe seu email..."
               />
+              <Form.ErrorMessage field="email" />
             </Form.Field>
-          </Form.Root>
 
-          <Form.Root>
             <Form.Field className="mt-4">
-              <Form.Label htmlFor="senha">Senha</Form.Label>
+              <Form.Label htmlFor="password">Senha</Form.Label>
               <Form.Input
-                name="senha"
+                name="password"
                 type="password"
                 placeholder="Informe sua senha...."
               />
+              <Form.ErrorMessage field="password" />
             </Form.Field>
+
+            {/* TODO refazer esse button com composition patter */}
+            <Button.Root type="submit" disabled={isSaving}>
+              {isSaving ? <p> Cadastrando...</p> : <p>Cadastrar</p>}
+            </Button.Root>
           </Form.Root>
         </div>
       </FormProvider>
-
-      {/* TODO adicionar botões */}
     </>
   );
 }
