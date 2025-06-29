8bb9a74ae04928be38555912801ed80e8a0aecbd
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-29 18:32:20 -0300

    refactor: transform index.tsx in index.ts

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -2,10 +2,12 @@ import { Field } from "./Field";
 import { Label } from "./Label";
 import { Root } from "./Root";
 import { Input } from "./Input";
+import { ErrorMessage } from "./ErrorMessage";
 
 export const Form = {
   Root,
   Field,
   Label,
   Input,
+  ErrorMessage,
 };
