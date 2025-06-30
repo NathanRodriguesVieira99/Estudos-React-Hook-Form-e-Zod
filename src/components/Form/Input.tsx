0e9c668812e106bf10616d5ee1cccf3b11a68062
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-29 21:01:13 -0300

    refactor: remove password visibility toggle logic from Input component

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,4 +1,4 @@
-import { useState, type ComponentProps } from 'react';
+import { type ComponentProps } from 'react';
 import { useFormContext } from 'react-hook-form'; // importa o contexto do React Hook Form
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
@@ -8,14 +8,7 @@ interface InputProps extends ComponentProps<'input'> {
 }
 
 export const Input = ({ name, placeholder, ...props }: InputProps) => {
-  const [showPassword, setShowPassword] = useState(false); // estado para gerenciar a exibição da senha (começa como false pra não mostrar)
-  const isPassword = props.type === 'password'; // verifica se o tipo do input (vindo de ...props) é password
   const { register } = useFormContext(); // chama o register usando o contexto do react hook form
-
-  // função que altera o estado de exibição da senha (de false pra true e vice-versa)
-  const handleShowPassword = () => {
-    setShowPassword((prev) => !prev);
-  };
 
   return (
     <div>
@@ -25,14 +18,7 @@ export const Input = ({ name, placeholder, ...props }: InputProps) => {
         placeholder={placeholder}
         {...register(name)} // conecta o input ao react hook form usando a prop name
         {...props}
-        type={isPassword ? (showPassword ? 'text' : 'password') : props.type} // se o input for do tipo password o input vira do tipo text e exibe a senha, caso contrario continua sendo password e não exibe
       />
-      {/* renderiza o botão com os ícones de exibição de senha caso o estado isPassword seja true */}
-      {isPassword && (
-        <button type="button" onClick={handleShowPassword} className="m-2">
-          {showPassword ? <FaEye /> : <FaEyeSlash />}
-        </button>
-      )}
     </div>
   );
 };
