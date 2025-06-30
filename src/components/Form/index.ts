409d34e2f30cf274edda56fcc0f5f13afeb7ddfa
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-29 21:02:00 -0300

    feat: add ShowPasswordToggle to Form component exports

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -1,13 +1,15 @@
-import { Field } from "./Field";
-import { Label } from "./Label";
-import { Root } from "./Root";
-import { Input } from "./Input";
-import { ErrorMessage } from "./ErrorMessage";
+import { Field } from './Field';
+import { Label } from './Label';
+import { Root } from './Root';
+import { Input } from './Input';
+ import { ShowPasswordToggle } from './ShowPasswordToggle';
+import { ErrorMessage } from './ErrorMessage';
 
 export const Form = {
   Root,
   Field,
   Label,
   Input,
+   ShowPasswordToggle,
   ErrorMessage,
 };
